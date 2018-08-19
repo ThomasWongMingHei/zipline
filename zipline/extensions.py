8a992cfcf298bd87afc11403a17813fccf73fba5
Thomas Wong <38015348+ThomasWongMingHei@users.noreply.github.com>
2018-08-19 14:35:21 +0100

    Update extensions.py

--- zipline/extensions.py
+++ zipline/extensions.py
@@ -270,8 +270,3 @@ extensible = create_registry
 custom_types = {}
 
 
-from zipline.data.bundles import register
-from zipline.data.bundles.csvdir import csvdir_equities
-start_session = pd.Timestamp('2012-1-1', tz='utc')
-end_session = pd.Timestamp('2013-1-1', tz='utc')
-register('custom-csvdir-bundle',csvdir_equities(["daily"],'D:\Quantopian'),calendar_name='NYSE',start_session=start_session,end_session=end_session)
